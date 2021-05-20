3a62b98cbb066f38ce2ae0adef2562e10b8d7075
Lokesh_Tangella <tangella.lokesh@gmail.com>
2021-05-20 11:18:15 +0530

    trying with @async

--- src/main/java/com/projectreveal/reveal/backend/BackendApplication.java
+++ src/main/java/com/projectreveal/reveal/backend/BackendApplication.java
@@ -3,9 +3,11 @@ package com.projectreveal.reveal.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.Async;
 
 @SpringBootApplication
 @ServletComponentScan
+@Async
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/projectreveal/reveal/backend/Config.java
+++
@@ -1,8 +0,0 @@
-package com.projectreveal.reveal.backend;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class Config {
-
-}
